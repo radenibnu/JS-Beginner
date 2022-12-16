6cf2f2d68f14de71636cc0c184827d2ab01d23ac
radenibnu <radenibnuqc@gmail.com>
2022-12-16 15:48:37 +0700

    first commit

---
+++ kode.js
@@ -0,0 +1,16 @@
+// this is comment
+
+// sentence = "how are you today?"
+
+//operators
+// buah = 20
+// persen = 0.2
+// fee = buah * persen
+
+// console.log(fee);
+
+buah = prompt("berapa harga buahnya ?");
+persen = prompt("berapa persen feenya ? %") / 100;
+total = buah * persen;
+
+console.log(total);
