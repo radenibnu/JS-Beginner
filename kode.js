7d2f58241a8357edccdedbaea21bc702d55a860f
radenibnu <radenibnuqc@gmail.com>
2022-12-17 07:37:49 +0700

    lesson 2 is done

--- kode.js
+++ kode.js
@@ -9,10 +9,50 @@
 
 // console.log(fee);
 
-hargaBuah = Number(prompt("berapa harga buahnya ?"));
-fee = Number(prompt("berapa persen feenya ? %")) / 100;
-fee = hargaBuah * fee;
-total = hargaBuah + fee;
+// hargaBuah = Number(prompt("berapa harga buahnya ?"));
+// fee = Number(prompt("berapa persen feenya ? %")) / 100;
+// fee = hargaBuah * fee;
+// total = hargaBuah + fee;
 
-console.log(" Total tip = ", fee);
-console.log("Harga = ", total);
+// console.log(" Total tip = ", fee);
+// console.log("Harga = ", total);
+
+//user input -> prompt
+
+/* data type (number, string) 
+interge -> 1, 20, 4.5
+string -> 'ini string', "ini juga string"
+array -> []
+object -> {}
+boolean -> false, true
+*/
+
+/* Math Operations
+multiply *
+divide /
+exponents ** 2 ** 5 = 2x2x2x2
+modulo/remainder %
+add +
+substract -
+*/
+
+/* Math Methods
+math floor -> 100.25 = 100. dibulatkan kebawah
+math ceil -> 100.25 = 101. dibulatkan keatas
+random -> give you random number
+*/
+
+//Baby weather app (conditional)
+// if ran -> 'Grab your umbrella'
+// else -> 'wear your sunglasses'
+
+let weather = prompt('How is the weather ?')
+
+if (weather == 'rain'){
+    console.log('Grab your umbrella')
+} else {
+    console.log('Wear your Sunglasses')
+}
+
+//conditional operators
+// ==, ===, >, <, <=, >=, !=, !==
