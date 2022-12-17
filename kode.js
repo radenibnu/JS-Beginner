7cebd0a0b307c69fc8f4b831f0cb4399d17fee06
radenibnu <radenibnuqc@gmail.com>
2022-12-17 11:12:31 +0700

    chapter 3 donne

--- kode.js
+++ kode.js
@@ -46,13 +46,63 @@ random -> give you random number
 // if ran -> 'Grab your umbrella'
 // else -> 'wear your sunglasses'
 
-let weather = prompt('How is the weather ?')
+// let weather = prompt('How is the weather ?')
 
-if (weather == 'rain'){
-    console.log('Grab your umbrella')
-} else {
-    console.log('Wear your Sunglasses')
-}
+// if (weather == 'rain'){
+//     console.log('Grab your umbrella')
+// } else {
+//     console.log('Wear your Sunglasses')
+// }
 
 //conditional operators
 // ==, ===, >, <, <=, >=, !=, !==
+
+//functions
+// this is a function calles 'sayMyName and it has 0 arguments
+// dose: it logs out your name to the console
+
+// function sayMyName(name){
+//     console.log(name);
+// }
+
+// sayMyName('ibnu')
+
+// this is a function called "greeting"
+// it has 1 argumetn called 'name
+// does: it logs out your nam to the concole
+
+function greeting(name){
+    // greet = 'hi ' + name + ', Nice to meet you!'
+    //template literals ``
+    greet = `hi ${name}, Nice to meet you`
+    console.log(greet)
+}
+
+greeting('rooney')
+
+function sum(a, b){
+    return a * b
+}
+
+function calculateFoodTotal(food, tip){
+    const tipPercentage = tip / 100
+    const tipAmount = food * tipPercentage
+    const total = sum(food, tipAmount)
+    
+    return total
+}
+
+console.log(calculateFoodTotal(100,20))
+
+//ES6 
+// arrow functions =>
+// arrow function with expilicit return
+const sumArrow = (a,b) => {
+    return a +b
+}
+
+// arrow function with implicit return
+// IMPORTANT : for implicit return, remove curly braces
+const sumArrow2 = (a,b) => a+b
+
+console.log(sumArrow2(50, 10))
